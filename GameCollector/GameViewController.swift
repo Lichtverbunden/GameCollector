6f0c094a313894da26442434b20e5a870272fd26
Ken Krippeler <kriken19@gmail.com>
2016-09-01 01:43:18 +0200

    All features in, besides camera.

--- GameCollector/GameViewController.swift
+++ GameCollector/GameViewController.swift
@@ -12,14 +12,30 @@ class GameViewController: UIViewController, UIImagePickerControllerDelegate, UIN
 {
     @IBOutlet weak var gameImageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var addUpdateButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
     
     var imagePicker = UIImagePickerController()
+    var game : Game? = nil
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
 
         imagePicker.delegate = self
+        
+        if game != nil
+        {
+            gameImageView.image = UIImage(data: game!.image as! Data)
+            titleTextField.text = game!.title
+            
+            addUpdateButton.setTitle("Update", for: .normal)
+        }
+        else
+        {
+            deleteButton.isHidden = true
+        }
+        
     }
 
     @IBAction func photosTapped(_ sender: AnyObject)
@@ -45,14 +61,33 @@ class GameViewController: UIViewController, UIImagePickerControllerDelegate, UIN
 
     @IBAction func addTapped(_ sender: AnyObject)
     {
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        
-        let game = Game(context: context)
-        game.title = titleTextField.text
-        game.image =  NSData(data: UIImagePNGRepresentation(gameImageView.image!)!)
+        if game != nil
+        {
+            game!.title = titleTextField.text
+            game!.image =  NSData(data: UIImagePNGRepresentation(gameImageView.image!)!)
+        }
+        else
+        {
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            
+            let game = Game(context: context)
+            game.title = titleTextField.text
+            game.image =  NSData(data: UIImagePNGRepresentation(gameImageView.image!)!)
+        }
         
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         
         navigationController!.popViewController(animated: true)
     }
+    
+    @IBAction func deleteTapped(_ sender: AnyObject)
+    {
+         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        context.delete(game!)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+    }
+    
 }
